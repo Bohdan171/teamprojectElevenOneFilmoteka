9556997fb0dcda2b29ef0f4ca8504b37892384b0
MariiaLutska <myklush@ukr.net>
2022-07-20 15:57:31 +0300

    preloader

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,17 +1,7 @@
-const maskEl = document.querySelector(".mask");
-const spinner = document.querySelectorAll(".inner");
-
-export function openLoading() {
-    maskEl.classList.remove('hide');
-    spinner.forEach(spin => {
-        spin.classList.remove('hide');
-    });
-}
-export function closeLoading() {
-    setInterval(() => {
-        maskEl.classList.add('hide');
-        spinner.forEach(spin => {
-            spin.classList.add('hide');
-        });
-    }, 1000);
-}
+ window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+  }
