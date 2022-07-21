9460343166fad6ad7bee60e1d5f47202a2c2c10d
MarinaHalahan <ichmarina1@gmail.com>
2022-07-21 20:02:58 +0300

    пагинация

---
+++ src/js/fetchfilm/fetch.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie'
+
+export default class NewFilms{ 
+    constuctor(){
+      this.name = ""  
+    }
+   
+         get query(){
+        return this.name
+      }
+      set query(newQuary){
+        this.name = newQuary
+      }
+    
+    async  fetch(page) {
+      try {
+        const keyapi = '4bceebe1d1f9bc99c966449bdeaecb86';
+        const requestparams = `?api_key=${keyapi}&language=en-US&page=${page}&include_adult=false&query=${this.name}`
+        const url = BASE_URL + requestparams;
+        
+        return await axios.get(`${url}`).then(response => response.data);}
+    catch (err) {
+      console.log(err);
+      throw err;
+    }
+
+     }
+    
+    }
