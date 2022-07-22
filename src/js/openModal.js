3b24730c4598a0bba6263075d1612d62b0bab68d
the-sneg <the.sneg.sasha@gmail.com>
2022-07-22 14:48:52 +0300

    fix_some_bug

--- src/js/openModal.js
+++ src/js/openModal.js
@@ -1,6 +1,5 @@
 import fetchGenre from './popular/fetchPopular.js';
 import { refs } from './refs.js';
-import aboutMovieTemplates from '../tmp/modalAboutFilm.hbs';
 import { localStorageAPI } from './localStorageAPI.js';
 import { closeOnClick, modalKeypressEsc } from './modalClose.js';
 // const newsPictureApi = new Fetch();
@@ -28,15 +27,20 @@ function onOpenModal(id) {
   document.body.classList.add('modal-open');
 
   fetchGenre.searchByMovieId(id).then(movie => {
-    
     const w = localStorageAPI.check(localStorageAPI.KEYS.WATCHED, movie);
     const q = localStorageAPI.check(localStorageAPI.KEYS.QUEUE, movie);
     if (w) {
-      document.querySelector('.js-modal-btn-watched').classList.toggle('visually-hidden');
-      document.querySelector('.js-modal-btn-remove-watched').classList.toggle('visually-hidden');
+      document
+        .querySelector('.js-modal-btn-watched')
+        .classList.toggle('visually-hidden');
+      document
+        .querySelector('.js-modal-btn-remove-watched')
+        .classList.toggle('visually-hidden');
     }
 
-    document.querySelector('.js-modal-btn-watched').addEventListener('click', onWatchedAdd);
+    document
+      .querySelector('.js-modal-btn-watched')
+      .addEventListener('click', onWatchedAdd);
     function onWatchedAdd(event) {
       event.target.classList.toggle('visually-hidden');
       event.target.nextElementSibling.classList.toggle('visually-hidden');
@@ -53,28 +57,33 @@ function onOpenModal(id) {
     }
 
     if (q) {
-      document.querySelector('.js-modal-btn-queue').classList.toggle('visually-hidden');
-      document.querySelector('.js-modal-btn-remove-queue').classList.toggle('visually-hidden');
+      document
+        .querySelector('.js-modal-btn-queue')
+        .classList.toggle('visually-hidden');
+      document
+        .querySelector('.js-modal-btn-remove-queue')
+        .classList.toggle('visually-hidden');
     }
 
-    document.querySelector('.js-modal-btn-queue').addEventListener('click', onQueueAdd);
+    document
+      .querySelector('.js-modal-btn-queue')
+      .addEventListener('click', onQueueAdd);
     function onQueueAdd(event) {
       event.target.classList.toggle('visually-hidden');
       event.target.nextElementSibling.classList.toggle('visually-hidden');
       localStorageAPI.set(localStorageAPI.KEYS.QUEUE, movie);
     }
 
-    document.querySelector('.js-modal-btn-remove-queue').addEventListener('click', onQueueRemove);
+    document
+      .querySelector('.js-modal-btn-remove-queue')
+      .addEventListener('click', onQueueRemove);
     function onQueueRemove(event) {
       event.target.classList.toggle('visually-hidden');
       event.target.previousElementSibling.classList.toggle('visually-hidden');
       localStorageAPI.delete(localStorageAPI.KEYS.QUEUE, movie);
     }
-    document.querySelector('.modal-img-play')
+    document
+      .querySelector('.modal-img-play')
       .addEventListener('click', watchTrailer);
-
   });
-
 }
-
-
