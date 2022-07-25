62db4a48051e41f335c8e7872faa7c12d2e9f119
MarinaHalahan <ichmarina1@gmail.com>
2022-07-25 19:43:54 +0300

    modalDefault / feature
    добавляет Other , если значение заголовков , жанров Undefined

--- src/js/markupCard.js
+++ src/js/markupCard.js
@@ -1,5 +1,5 @@
 export  function createMarkup(results) {
-  const markup = results.map(({id, title, poster_path, release_date = "2021-01-01", genre_ids }) => {
+  const markup = results.map(({id, title="", name="", poster_path, release_date = "2021-01-01", genre_ids }) => {
         const url = `https://image.tmdb.org/t/p/w500`
         const date = release_date.slice(0, 4)
         const genreItems = JSON.parse(localStorage.getItem("genre")).genres
@@ -8,17 +8,20 @@ export  function createMarkup(results) {
           for (const el of genre_ids){
           if (genre.id === el){
             newName.push(genre.name)} }          
-        }
-         genre_ids = newName.slice(0,2)
+        }console.log(genre_ids);
+    genre_ids = newName.slice(0, 2);
+    if (genre_ids.length === 0) {
+       genre_ids="Other";
+    }
         return `
       
        <div  class="film-card gallery-items" data-modal="${id}">
        <a class="gallery__link" href="${url}${poster_path}">
-       <img class="gallery__image js-card-img" src="${url}${poster_path}" alt="${title}" loading="lazy" />
+       <img class="gallery__image js-card-img" src="${url}${poster_path}" alt="${title}${name}" loading="lazy" />
        </a>
        <div class="info">
          <h2 class="title">
-            ${title}
+            ${title}${name}
          </h2>
          <p class="info-description">
          <b class="genre">${genre_ids} </b>
