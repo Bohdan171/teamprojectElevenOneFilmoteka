4adf3c6c6acae40b5f20d6ac5a1cbfa3db7a07a6
AlinaSenkovich <alinasenkovich33@gmail.com>
2022-07-27 00:06:10 +0300

    dark/css

--- src/js/markupModalCard.js
+++ src/js/markupModalCard.js
@@ -1,11 +1,7 @@
 // const modal = document.querySelector('.js-modal');
 export function makeModalCard({ id, title, poster_path,overview,vote_average,vote_count,popularity,release_date = "2021-01-01" },data) {
     let genre_ids = data;
-    return `<button type="button" class="modal-close-btn js-close-btn">
-      <svg class="modal-close-btn__icon" width="14" height="14">
-        <use href='/src/images/sprite.svg#icon-close'></use>
-      </svg>
-    </button>
+    return `<button type="button" class="modal-close-btn js-close-btn" width="14" height="14">×</button>
     <div class="card-container">
       <div class='modal-wrapper js-modal-wrapper' data-id='${id}'>
         <div class='wrapper-img'>
@@ -15,7 +11,6 @@ export function makeModalCard({ id, title, poster_path,overview,vote_average,vot
             alt='moviе poster: ${title}'
           />
           <div class='overlay'>
-            <img class='modal-img-play' src="/images/play-orange.png" alt='icon-play'/>
           </div>
         </div>
         <div class='modal-right-part'>
@@ -28,7 +23,7 @@ export function makeModalCard({ id, title, poster_path,overview,vote_average,vot
             <li class='modal-list__info--item'><h3 class='modal-list__info--title'>Genre</h3><p class='modal-list__content--item'>${genre_ids}</p></li>
           </ul>
           </div>
-          <h3 class='modal-secondary-title'>About</h3>
+          <b class='modal-secondary-title'>About</b>
           <p class='modal-film-description'>${overview}</p>
           <div class='modal-wrapper-btn'>
             <button class='modal-btn modal-btn-watched js-modal-btn-watched' type='button'>add to watched</button>
