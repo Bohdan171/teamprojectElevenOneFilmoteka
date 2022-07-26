b5e5e75e6eaf33441b4b09a8ca88240ae5540ec0
the-sneg <the.sneg.sasha@gmail.com>
2022-07-26 20:04:16 +0300

    Update index.js

--- src/js/fetchfilm/index.js
+++ src/js/fetchfilm/index.js
@@ -1,54 +1,59 @@
 import NewFilms from './fetch.js';
-import {fetchGenre} from './genre.js';
+import { fetchGenre } from './genre.js';
 
-const form = document.querySelector(".search-form");
+const form = document.querySelector('.search-form');
 const input = document.querySelector('.search-field');
-const gallery = document.querySelector(".gallary");
+const gallery = document.querySelector('.gallary');
 console.log(gallery);
 console.log(input);
 export let is_search = false;
-export let query_line = "";
+export let query_line = '';
 const newFilms = new NewFilms();
 
 console.log(newFilms);
 
-form.addEventListener("submit", onSubmit);
+form.addEventListener('submit', onSubmit);
 
-async function onSubmit(evt){
-    evt.preventDefault();
-    fetchGenre();
-    is_search = true;  
+async function onSubmit(evt) {
+  evt.preventDefault();
+  fetchGenre();
+  is_search = true;
   query_line = input.value.trim();
   newFilms.name = query_line;
-    newFilms.fetch(1)
-  .then(data => {
-    return data.results;
-  })
-  .then(results => {gallery.innerHTML = createMarkup(results);});
+  newFilms
+    .fetch(1)
+    .then(data => {
+      return data.results;
+    })
+    .then(results => {
+      gallery.innerHTML = createMarkup(results);
+    });
 }
 
-    
-
-function createMarkup(results){
-    const markup = results.map(({title, poster_path, release_date = "2021-01-01", genre_ids}) => {
-        const url = `https://image.tmdb.org/t/p/w500`;
-        const date = release_date ? release_date.slice(0, 4) : 'No information';
-        const genreItems = JSON.parse(localStorage.getItem("genre")).genres;
-          let newName = [];
-        for (const genre of genreItems){
-          for (const el of genre_ids){
-          if (genre.id === el){
-            newName.push(genre.name);} }          
+function createMarkup(results) {
+  const markup = results
+    .map(({ title, poster_path, release_date = '2021-01-01', genre_ids }) => {
+      const url = `https://image.tmdb.org/t/p/w500`;
+      const date = release_date ? release_date.slice(0, 4) : 'No information';
+      const genreItems = JSON.parse(localStorage.getItem('genre')).genres;
+      let newName = [];
+      for (const genre of genreItems) {
+        for (const el of genre_ids) {
+          if (genre.id === el) {
+            newName.push(genre.name);
+          }
+        }
       }
       genre_ids = newName.slice(0, 2);
-        return `
+      return `
         
        <div class="film-card">
        <a class="gallery__link" href="${url}${poster_path}">
        <img class="gallery__image" src=${
-          poster_path !== null
-            ? `https://image.tmdb.org/t/p/w500${poster_path}`
-            : `https://mysteriouswritings.com/wp-content/uploads/2017/02/movie.jpg`} alt="${title}" loading="lazy" />
+         poster_path !== null
+           ? `https://image.tmdb.org/t/p/w500${poster_path}`
+           : `https://www.csaff.org/wp-content/uploads/csaff-no-poster.jpg`
+       } alt="${title}" loading="lazy" />
        </a>
        <div class="info">
          <h2 class="title">
@@ -64,7 +69,8 @@ function createMarkup(results){
          </p>
          
        </div>
-     </div>`;}).join("");
-    return markup;
-     
- }
+     </div>`;
+    })
+    .join('');
+  return markup;
+}
