13df0956aaf77b5001674cda776d7a09d0c5e985
MariiaLutska <myklush@ukr.net>
2022-07-27 18:18:27 +0300

    fix

--- src/js/fetchfilm/index.js
+++ src/js/fetchfilm/index.js
@@ -1,6 +1,5 @@
 import NewFilms from './fetch.js';
 import { fetchGenre } from './genre.js';
-import { createMarkup } from '../markupCard.js';
 
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const form = document.querySelector('.search-form');
@@ -32,3 +31,48 @@ async function onSubmit(evt) {
     });
 }
 
+function createMarkup(results) {
+  const markup = results
+    .map(
+      ({ id, title, poster_path, release_date = '2021-01-01', genre_ids }) => {
+        const url = `https://image.tmdb.org/t/p/w500`;
+        const date = release_date ? release_date.slice(0, 4) : 'No information';
+        const genreItems = JSON.parse(localStorage.getItem('genre')).genres;
+
+        let genreNames = genreItems
+          .filter(genre => genre_ids.includes(genre.id))
+          .map(genre => genre.name);
+        if (genreNames.length > 2) {
+          genreNames = genreNames.slice(0, 2).concat(['other...']);
+        } else if (genreNames.length === 0) {
+          genreNames = ['No information'];
+        }
+        genreNames = genreNames.join(', ');
+
+        return `
+      <div class="film-card">
+      <a class="gallery__link" href="${url}${poster_path}">
+      <img class="gallery__image" src=${
+        poster_path !== null
+          ? `https://image.tmdb.org/t/p/w500${poster_path}`
+          : `https://www.csaff.org/wp-content/uploads/csaff-no-poster.jpg`
+      } alt="${title}" loading="lazy" />
+        </a>
+        <div class="info">
+        <h2 class="title">
+        ${title}
+        </h2>
+        <p class="info-description">
+        <b class="genre"> ${genreNames} </b>
+        <b> | </b>
+        <b class="yers">
+        ${date}
+        </b>
+        </p>
+        </div>
+        </div>`;
+      }
+    )
+    .join('');
+  return markup;
+}
--- src/js/markupCard.js
+++ src/js/markupCard.js
@@ -1,45 +1,49 @@
-export  function createMarkup(results) {
+export function createMarkup(results) {
   const markup = results
-    .map(({ title, poster_path, release_date = '2021-01-01', genre_ids }) => {
-      const url = `https://image.tmdb.org/t/p/w500`;
-      const date = release_date ? release_date.slice(0, 4) : 'No information';
-      const genreItems = JSON.parse(localStorage.getItem('genre')).genres;
+    .map(
+      ({
+        id,
+        title = '',
+        name = '',
+        poster_path,
+        release_date = '2021-01-01',
+        genre_ids = [],
+      }) => {
+        const url = `https://image.tmdb.org/t/p/w500`;
+        const date = release_date.slice(0, 4);
+        const genreItems = JSON.parse(localStorage.getItem('genre')).genres;
 
-      let genreNames = genreItems.filter(genre => genre_ids.includes(genre.id))
-        .map(genre => genre.name);
-      if (genreNames.length > 2) {
-        genreNames = genreNames.slice(0, 2).concat(['other...']);
+        let genreNames = genreItems
+          .filter(genre => genre_ids.includes(genre.id))
+          .map(genre => genre.name);
+        if (genreNames.length > 2) {
+          genreNames = genreNames.slice(0, 2).concat(['other...']);
+        } else if (genreNames.length === 0) {
+          genreNames = ['No information'];
+        }
+        genreNames = genreNames.join(', ');
+        return `
+      
+       <div  class="film-card gallery-items" data-modal="${id}">
+       <a class="gallery__link" href="${url}${poster_path}">
+       <img class="gallery__image js-card-img" src="${url}${poster_path}" alt="${title}${name}" loading="lazy" />
+       </a>
+       <div class="info">
+         <h2 class="title">
+            ${title}${name}
+         </h2>
+         <p class="info-description">
+         <b class="genre">${genreNames} </b>
+         <b> | </b>
+         <b class="yers">
+           ${date}
+         </b>
+         </p>
+         
+       </div>
+     </div>`;
       }
-      else if (genreNames.length === 0) {
-        genreNames = ['No information'];
-      }
-      genreNames = genreNames.join(', ');
-
-      return `
-
-      <div class="film-card">
-      <a class="gallery__link" href="${url}${poster_path}">
-      <img class="gallery__image" src=${
-        poster_path !== null
-        ? `https://image.tmdb.org/t/p/w500${poster_path}`
-          : `https://www.csaff.org/wp-content/uploads/csaff-no-poster.jpg`
-        } alt="${title}" loading="lazy" />
-        </a>
-        <div class="info">
-        <h2 class="title">
-        ${title}
-        </h2>
-        <p class="info-description">
-        <b class="genre"> ${genreNames} </b>
-        <b> | </b>
-        <b class="yers">
-        ${date}
-        </b>
-        </p>
-
-        </div>
-        </div>`;
-    })
+    )
     .join('');
   return markup;
-};
+}
